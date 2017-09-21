520af9060bf6ec1096267d925ab5ba358cb9bdef
Noah KI <noahkeedleisack@gmail.com>
2017-09-21 22:21:24 +0100

    transfer

---
+++ src/Buffer.h
@@ -0,0 +1,17 @@
+
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <string>
+
+typedef struct Buffer {
+    Buffer();
+
+    std::string getBetween(int line1, int line2) {
+        
+    };
+
+    std::string contents;
+} Buffer;
+
+#endif
---
+++ src/Editor.h
@@ -0,0 +1,14 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+#include "Buffer.h"
+
+typedef struct Editor {
+
+    Editor();
+
+    Buffer textBuffer;
+
+} Editor;
+
+#endif
